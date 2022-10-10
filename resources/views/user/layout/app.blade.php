b348b7080d35ebd0e692cd2916e1d0f508b18910
mainstar2021 <mainstar2021@gmail.com>
2022-10-10 16:25:32 -0400

    Update app.blade.php

--- resources/views/user/layout/app.blade.php
+++ resources/views/user/layout/app.blade.php
@@ -39,13 +39,13 @@
 
     </div>
 
-    <div class="loading-overlay">
+    {{-- <div class="loading-overlay">
         <div class="bounce-loader">
             <div class="bounce1"></div>
             <div class="bounce2"></div>
             <div class="bounce3"></div>
         </div>
-    </div>
+    </div> --}}
 
     <div class="mobile-menu-overlay"></div>
     <!-- End .mobil-menu-overlay -->
